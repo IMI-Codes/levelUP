b6c913f943d0c15e77fa568c779e71d652c4e697
Manasseh <rougerapper@gmail.com>
2024-07-25 20:04:37 +0100

    started modeling the workout app classes

---
+++ Leveling/WorkoutTracker/cls/main.py
@@ -0,0 +1,14 @@
+class workout:
+  def __init__(self):
+    self.day = None
+    self.type = None
+    self.numberOfExercise = None
+
+class exercise:
+  def __init__(self):
+    self.exerciseName = None
+    self.setsToComplete = None
+    self.repRange = None
+    self.setsCompleted = None
+    self.repsForSetCompleted = None
+
